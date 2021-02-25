8418e4524f88075cadf3661e9c5b6f11740aa51d
mirco verderosa <verderosamircowork@gmail.com>
2021-02-25 16:25:51 +0100

    creata variabile con la badword,poi sostituita con asterischi tramite replace

--- index.php
+++ index.php
@@ -6,15 +6,43 @@
   </head>
   <body>
 
-  <?php
 
-  $paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+   <?php
+   $badWord = $_GET['badword'];
+   echo $badWord;
 
-echo $paragraph;
-  ?>
-  <br>
-  <br>
-       La lunghezza della stringa è : <?php echo strlen($paragraph);?>
+    ?>
+
+    <?php
+
+    $paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+
+    $paragraph = str_replace('adipisicing',$badWord,$paragraph);
+    var_dump($_GET);
+
+    $paragraph = str_replace($badWord,'***',$paragraph);
+
+
+    ?>
+    <br>
+    <br>
+
+    <p><?php echo $paragraph ?></p>
+
+      <div class="lorem-length">
+      <h1>La lunghezza della stringa è : <?php echo strlen($paragraph);?> caratteri</h1>
+      </div>
+
+         <br>
+         <br>
+
+     <?php
+
+
+     ?>
+     <br>
+     <br>
+
 
   </body>
 </html>
